e381abb936ad63b417d38a2bf4236534b79f80ae
Yauheni Mileika <vip.carich@gmail.com>
2023-09-14 02:11:17 +0300

    LAB-01: create django app

---
+++ CocoPizza/myshop/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name='shop'
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+        path('accounts/', include('django.contrib.auth.urls')),
+    path('home/', include('users.urls')),
+    path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
+    path('', include(('shop.urls','shop'), namespace='shop'))
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
